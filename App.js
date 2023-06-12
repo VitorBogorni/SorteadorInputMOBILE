1ee7f09bf15d9fe7f2c518a95bc3eee1d1d15b32
VitorBogorni <112990035+VitorBogorni@users.noreply.github.com>
2023-06-12 20:46:59 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View } from "react-native";
+
+import Projeto from "./componentes/Projeto";
+import Topo from "./componentes/Topo";
+
+export default function App() {
+  return (
+    <View>
+      <Topo />
+      <Projeto />
+    </View>
+  );
+}
